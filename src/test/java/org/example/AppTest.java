6a7474ed7039c71ec2845473c93fdf6cdbf86a1a
lyjady <lyj2780866735>
2020-01-23 17:40:49 +0800

    Initial commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,110 @@
+package org.example;
+
+
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+public class AppTest {
+
+    private ZooKeeper zooKeeper;
+
+    /**
+     * 1.创建Zookeeper连接
+     * @throws IOException
+     */
+    @Before
+    public void connectZooKeeperServer() throws IOException {
+        zooKeeper = new ZooKeeper("192.168.0.110:2181", 200000, new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                System.out.println("path: " + event.getPath());
+                System.out.println("state: " + event.getState());
+                System.out.println("type: " + event.getType());
+                System.out.println("wrapper: " + event.getWrapper());
+            }
+        }, false);
+    }
+
+    /**
+     * 2.创建父节点
+     */
+    @Test
+    public void createNode() throws KeeperException, InterruptedException {
+        String spring = zooKeeper.create("/Spring", "change the java".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        //返回节点的路径
+        System.out.println(spring);
+    }
+
+    /**
+     * 3.创建子节点
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    @Test
+    public void createChildrenNode() throws KeeperException, InterruptedException {
+        String spring = zooKeeper.create("/Spring/SpringMVC", "change the controller".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        //返回节点的路径
+        System.out.println(spring);
+    }
+
+    /**
+     * 4.获取节点的值
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    @Test
+    public void getNodeValue() throws KeeperException, InterruptedException {
+        //用于接收节点的信息(创建时间、数据版本等)对象
+        Stat stat = new Stat();
+        byte[] fatherData = zooKeeper.getData("/Spring", false, stat);
+        byte[] sonData = zooKeeper.getData("/Spring/SpringMVC", false, null);
+        System.out.println(new String(fatherData));
+        System.out.println(new String(sonData));
+    }
+
+    /**
+     * 5.设置节点的值
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    @Test
+    public void setNodeValue() throws KeeperException, InterruptedException {
+        zooKeeper.setData("/Spring", "Spring".getBytes(), -1);
+    }
+
+    /**
+     * 6.判断某个节点是否存在
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    @Test
+    public void isExists() throws KeeperException, InterruptedException {
+        Stat spring = zooKeeper.exists("/Spring", false);
+        System.out.println(spring);
+    }
+
+    /**
+     * 7.删除节点(节点必须为空)
+     */
+    @Test
+    public void deleteNode() throws KeeperException, InterruptedException {
+        zooKeeper.delete("/a", -1);
+    }
+
+    /**
+     * 8.查询子节点
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    @Test
+    public void getChildrenNode() throws KeeperException, InterruptedException {
+        List<String> children = zooKeeper.getChildren("/Spring", false);
+        children.forEach(System.out::println);
+    }
+}
